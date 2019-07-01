82474f1aaf923590afa2157cd9c470fdf482e8a2
Wei Cheng Hung <s777610@gmail.com>
2019-06-30 18:42:31 -0700

    setup subscriber, pubsub on backend

--- resolvers.js
+++ resolvers.js
@@ -1,5 +1,10 @@
-const { AuthenticationError } = require("apollo-server");
+const { AuthenticationError, PubSub } = require("apollo-server");
 const Pin = require("./models/Pin");
+
+const pubsub = new PubSub();
+const PIN_ADDED = "PIN_ADDED";
+const PIN_DELETED = "PIN_DELETED";
+const PIN_UPDATED = "PIN_UPDATED";
 
 // This is auth middleware
 const authenticated = next => (parent, args, context, info) => {
@@ -28,11 +33,14 @@ module.exports = {
       }).save();
 
       const pinAdded = await Pin.populate(newPin, "author");
+
+      pubsub.publish(PIN_ADDED, { pinAdded });
       return pinAdded;
     }),
     deletePin: authenticated(async (parent, args, context, info) => {
       // exec() return the promise
       const pinDeleted = await Pin.findOneAndDelete({ _id: args.pinId }).exec();
+      pubsub.publish(PIN_DELETED, { pinDeleted });
       return pinDeleted;
     }),
     createComment: authenticated(async (parent, args, context, info) => {
@@ -46,7 +54,19 @@ module.exports = {
         .populate("author")
         .populate("comments.author");
 
+      pubsub.publish(PIN_UPDATED, { pinUpdated });
       return pinUpdated;
     })
+  },
+  Subscription: {
+    pinAdded: {
+      subscribe: () => pubsub.asyncIterator(PIN_ADDED)
+    },
+    pinDeleted: {
+      subscribe: () => pubsub.asyncIterator(PIN_DELETED)
+    },
+    pinUpdated: {
+      subscribe: () => pubsub.asyncIterator(PIN_UPDATED)
+    }
   }
 };
